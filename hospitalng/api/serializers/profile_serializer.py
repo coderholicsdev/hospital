2fc9adeb945c245a9c54a2dcfd7364777b6dd857
Joseph Braide <braidej@gmail.com>
2022-04-08 17:15:08 +0100

    removed needless Profileserializermixin

--- hospitalng/api/serializers/profile_serializer.py
+++ hospitalng/api/serializers/profile_serializer.py
@@ -3,16 +3,6 @@ from rest_framework import serializers
 # import models
 from api.models import PatientProfile, DoctorProfile, AdminProfile
 
-
-class ProfileSerializerMixin(serializers.ModelSerializer):
-    def validate(self, validated_data):
-        user = validated_data.get('user')
-        try:
-            self.Meta.model.objects.create(user=user)
-        except:
-            raise serializers.ValidationError('User with this account exists already')
-        return super().validate(validated_data)
-
 class PatientProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = PatientProfile
