abe4b8427a873f556c8bdd2a9cd5ed7ac6086f87
Joseph Braide <braidej@gmail.com>
2022-04-08 17:26:23 +0100

    added user to fields and used names instead of id

--- hospitalng/api/serializers/doctor_serializer.py
+++ hospitalng/api/serializers/doctor_serializer.py
@@ -7,6 +7,9 @@ from rest_framework import serializers
 from api.models import DoctorProfile
 
 class DoctorProfileSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source="user.email", read_only=True)
+    hospital_name = serializers.CharField(source='hospital_name.name', read_only=True)
+
     class Meta:
         model = DoctorProfile
-        fields = ['name', 'phone_number', 'qualifications', 'specialty_or_department', 'title', 'hospital_name', 'profile_picture']
+        fields = ['user', 'name', 'phone_number', 'qualifications', 'specialty_or_department', 'title', 'hospital_name', 'profile_picture']
