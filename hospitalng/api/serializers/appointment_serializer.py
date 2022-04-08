a0cfcff65ebfcf06a8f4132ee7accb0de4c01c3c
Joseph Braide <braidej@gmail.com>
2022-04-08 22:28:06 +0100

    refactoring appt serz. validators and meta class

--- hospitalng/api/serializers/appointment_serializer.py
+++ hospitalng/api/serializers/appointment_serializer.py
@@ -19,31 +19,33 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # import serializer model 
-from api.models import Appointment
+from api.models import Appointment, DoctorProfile
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    doctor = serializers.CharField(source='doctor.name')
     class Meta:
         model = Appointment
-        fields = ['appointment_id', 'created_by', 'date', 'time']
+        fields = ['appointment_id', 'doctor', 'date', 'time']
 
 class BookAppointmentSerializer(serializers.ModelSerializer):
-    appointment_with = serializers.CharField(allow_blank=False, allow_null=False)
+    doctor = serializers.CharField(allow_blank=False, allow_null=False)
     class Meta:
         model = Appointment
-        fields = ['appointment_with', 'date', 'time']
+        fields = ['doctor', 'date', 'time']
     
-    def validate_appointment_with(self, attrs):
+    def validate_doctor(self, attrs):
+        user = get_user_model().objects.get(email=attrs)
         try:
-            user = get_user_model().objects.get(email=attrs)
+            doctor = DoctorProfile.objects.get(user=user)
         except ObjectDoesNotExist:
             return Response(
                 data={
-                    'error': 'This user does not exist, try a different one'
+                    'error': 'This is not a valid Doctor Profile'
                 },
                 status=status.HTTP_404_NOT_FOUND
             )
-        attrs = user
+        attrs = doctor
         return super().validate(attrs)
 
     def validate_date(self, attrs):
