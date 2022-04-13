60270a5b6e60abf31b68861aeadfa742590b28d1
Joseph Braide <braidej@gmail.com>
2022-04-13 10:06:13 +0100

    included allowedhosts

--- hospitalng/hospitalng/settings.py
+++ hospitalng/hospitalng/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-3ej)xt2=r(f-y$a(!nzfz$yuc1*e#%l!h4mg!)*b9$fq_bv=_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['172.105.101.134', 'localhost', '127.0.0.1']
 
 # Application definition
 
