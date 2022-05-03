db2561bb38664ba1a5352e7e217f3575080162e5
Joseph Braide <braidej@gmail.com>
2022-05-03 06:14:39 +0100

    cors origin

--- hospitalng/hospitalng/settings.py
+++ hospitalng/hospitalng/settings.py
@@ -44,11 +44,16 @@ INSTALLED_APPS = [
 
     # third party packages
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    # cors middleware
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -194,3 +199,9 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+# CORS ALLOWED ORIGINS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4653",
+    "http://172.105.101.134",
+]
