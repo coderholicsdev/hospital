04d64d798eab8cced1ef1f50103c82bd43c6cf16
Joseph Braide <braidej@gmail.com>
2022-05-03 07:37:52 +0100

    Allowed all on cors

--- hospitalng/hospitalng/settings.py
+++ hospitalng/hospitalng/settings.py
@@ -201,11 +201,12 @@ SIMPLE_JWT = {
 }
 
 # CORS ALLOWED ORIGINS
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4653",
-    "http://172.105.101.134",
-    "http://127.0.0.1:5501",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:4653",
+#     "http://172.105.101.134",
+#     "http://127.0.0.1:5501",
+# ]
 
 # CORS_ALLOWED_ORIGINS = [
 #     '*'
