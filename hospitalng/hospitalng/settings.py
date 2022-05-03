4f2c288084bb0e63d7090aa76d74b4b898b8d068
Joseph Braide <braidej@gmail.com>
2022-05-03 06:43:38 +0100

    cors middleware

--- hospitalng/hospitalng/settings.py
+++ hospitalng/hospitalng/settings.py
@@ -48,11 +48,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-
     # cors middleware
     "corsheaders.middleware.CorsMiddleware",
+
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
